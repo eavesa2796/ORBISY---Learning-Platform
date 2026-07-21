3b7684d0d3cf2f376461a86beefaa68dd7c38b3f
Anthony Eaves <anthonyeaves33@gmail.com>
2026-07-21 09:43:55 -0500

    Connect learning page to multi-track hub

--- app/learning/page.tsx
+++ app/learning/page.tsx
@@ -1,14 +1,11 @@
-import InteractiveLesson from "@/components/InteractiveLesson";
+import LearningHub from "@/components/LearningHub";
 import SiteNav from "@/components/SiteNav";
-import { lessons } from "@/data/lessons";
 
 export default function LearningPage() {
-  const featuredLesson = lessons[0];
-
   return (
     <main className="shell shell-wide">
       <SiteNav title="ORBISY Learning Platform" />
-      <InteractiveLesson lesson={featuredLesson} />
+      <LearningHub />
     </main>
   );
 }
